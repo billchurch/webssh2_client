4f57e2ddb17e68e629651eb5dafe38b96eedc2b4
Bill Church <billchurch@users.noreply.github.com>
2024-07-17 15:27:49 +0000

    chore: Update webpack production configuration to optimize code size and performance

--- scripts/webpack.prod.js
+++ scripts/webpack.prod.js
@@ -1,22 +1,40 @@
 // scripts/webpack.prod.js
-const { merge } = require('webpack-merge')
-const TerserPlugin = require('terser-webpack-plugin')
-const common = require('./webpack.common.js')
+const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common.js');
 
-module.exports = merge(
-  {
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true
-            }
-          }
-        })
-      ]
-    }
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+          compress: {
+            drop_console: false, // Remove console statements for smaller file size
+            drop_debugger: false,
+            // Reduce the passes and compress options
+            passes: 1,
+            dead_code: true,
+            unused: true,
+          },
+          // Disable property mangling
+          mangle: {
+            properties: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
   },
-  common
-)
+  performance: {
+    hints: 'warning', // Show performance hints
+    maxEntrypointSize: 512000, // 512 KB
+    maxAssetSize: 512000, // 512 KB
+  },
+});
